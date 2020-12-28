9afaf612f6456697bff08af13a621df53a59a428
tajima <>
2020-12-28 16:07:56 +0900

    [add][update] headerのcssファイルを追加、headerのphpを変更、headerのphpにcssを適用

--- php/Header.php
+++ php/Header.php
@@ -1,12 +1,40 @@
 <header>
-  <h1 class="headline">
-    <a>サンプルのサイト</a>
-  </h1>
-  <ul class="nav-list">
-    <li class="nav-list-item">
-      <a>Home</a>
-    </li>
-    <li class="nav-list-item">About</li>
-    <li class="nav-list-item">Topic</li>
-  </ul>
+  <link rel="stylesheet" href="../css/header.css">
+  <!--ヘッダー部分-->
+  <div class="header">
+    <!--ヘッダー部分上-->
+    <div class="headline">
+      <span class="left">
+        ログインID:○○○○<br> <!--ID-->
+        名前：山田太郎      <!--name-->
+      </span>
+
+      <span class="center">
+        ○○勤務時間管理システム
+      </span>
+
+      <span class="right">
+        ページ更新<br>
+        2020/01/01 00:00 <!--time-->
+      </span>
+    </div>
+
+    <!--ヘッダー部分下-->
+    <table class="header_table">
+      <tr>
+        <td> 1 </td>
+        <td> 2 </td>
+        <td> 3 </td>
+        <td> 4 </td>
+      </tr>
+      <tr>
+        <td> 5 </td>
+        <td> 6 </td>
+        <td> 7 </td>
+        <td class="logout"> ログアウト </td>
+      </tr>
+    </table>
+
+  </div>
+  <!--ヘッダー部分終了-->
 </header>
