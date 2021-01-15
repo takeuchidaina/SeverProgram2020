f977cc3449f5124389be93372a596c3d2c5cfeaa
takeuchidaina <yda.takeuchidaina@gmail.com>
2021-01-15 15:06:07 +0900

    [update]ログイン画面にcssを適用

--- php/LogIn.php
+++ php/LogIn.php
@@ -7,9 +7,10 @@
 <body>
   <h2>〇〇勤務時間調整システム</h2>
 <form method="POST" action="AMS/php/SignIn.php">
-  <p>ID:<input type="text" value="" name="ID" maxlength="12"></p>
-  <p>パスワード:<input type="text" value="" name="Pass" maxlength="12"></p>
-  <input type="submit" value="ログイン">
+  <p><b class="string--bg">ID</b><input type="text" value="" name="ID" maxlength="12"></p>
+  <p><b class="string--bg">パスワード</b><input type="text" value="" name="Pass" maxlength="12"></p>
+  <b><button type="submit" onfocus="this.blur();" class="button--orange" autofocus=true>ログイン </button></b>
+
 </form>
 
   <?php
