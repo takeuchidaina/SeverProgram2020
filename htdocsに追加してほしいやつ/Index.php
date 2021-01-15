1396ed6ce27dc4b4fd5acdcc21f358855dbdac27
takeuchidaina <yda.takeuchidaina@gmail.com>
2021-01-15 15:05:49 +0900

    [update]indexでcss読み込み

--- htdocsに追加してほしいやつ/Index.php
+++ htdocsに追加してほしいやつ/Index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="AMS/css/CSSReader.css">
+<title>LogIn</title>
+</head>
+
+<body>
 <?php
 //define('_ROOT_DIR', __DIR__ . '/');
 //session_start();
@@ -16,3 +25,5 @@ setcookie($_COOKIE[session_name()],'',time()-1);//クッキー消去
 session_destroy();//セッション消去
 */
 ?>
+</body>
+</html>
