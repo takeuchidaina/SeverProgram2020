9b905299d575e213b73e57e57f0477003de02aed
takeuchidaina <yda.takeuchidaina@gmail.com>
2021-01-22 15:10:45 +0900

    [update]勤務時間統計　CSS適用

--- php/WorkTimeView.php
+++ php/WorkTimeView.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>WorkTimeView</title>
+<link rel="stylesheet" href="../css/CSSReader.css">
 </head>
 <body>
 <?php
@@ -11,7 +12,7 @@ require 'Header.php';
 
 
   <h3>ここから内容</h3>
-  <h3>勤務時間統計閲覧</h3>
+  <h3 class="string--color--lb">勤務時間統計閲覧</h3>
 
 
   <?php
