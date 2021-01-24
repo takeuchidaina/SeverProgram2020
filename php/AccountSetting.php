b3cde3dec6545cdf16709a444c1fb908add264d5
takeuchidaina <yda.takeuchidaina@gmail.com>
2021-01-24 13:40:05 +0900

    [update]CSS適用　アカウント設定

--- php/AccountSetting.php
+++ php/AccountSetting.php
@@ -3,21 +3,19 @@
 <head>
 <meta charset="UTF-8">
 <title>AccountSetting</title>
+<link rel="stylesheet" href="../css/CSSReader.css">
 </head>
 <body>
 <?php
 require 'Header.php';
 ?>
 
-
-  <h3>ここから内容</h3>
-  <h3>アカウント管理</h3>
+  <h3 class="string--color--lb">アカウント管理</h3>
   <form method="POST" action="AccountSet.php">
-    <p><b class="string--bg">現在のパスワード</b><input type="text" value="" name="OldPass" maxlength="12"></p>
-    <p><b class="string--bg">新しいパスワード</b><input type="text" value="" name="NewPass1" maxlength="12"></p>
-    <p><b class="string--bg">新しいパスワード</b><input type="text" value="" name="NewPass2" maxlength="12"></p>
-    <b><button type="submit" onfocus="this.blur();" class="button--orange" autofocus=true>変更 </button></b>
-
+    <p><b class="string--bg--fit">現在のパスワード</b><input type="text" value="" name="OldPass" maxlength="12"></p>
+    <p><b class="string--bg--fit">新しいパスワード</b><input type="text" value="" name="NewPass1" maxlength="12"></p>
+    <p><b class="string--bg--fit">新しいパスワード</b><input type="text" value="" name="NewPass2" maxlength="12"></p>
+    <b><button type="submit" onfocus="this.blur();" class="button--orange" autofocus=true>　変更　 </button></b>
   </form>
 
 
