40a963f40414dfe62d75d8d47c8c7df68ad42247
MellMag <yda.tm.2360623@gmail.com>
2021-02-03 19:38:53 +0900

    [update] 可変プルダウン作成

--- php/rensyu01.php
+++ php/rensyu01.php
@@ -6,12 +6,53 @@
 </head>
 <body>
 <?php
+$year = date('Y');
 $month = date("m");	 // 今日の月を取得する
 $day = date("d");	 // 今日の日を取得する
-echo "今日は $month 月 $day 日です<br>"; /*表示*/
+echo "$year 年 $month 月 $day 日<br>"; /*表示*/
 
+//debug
+//$month = 12;
+
+$month += 0;
+for($i=1 ; $i<4 ; $i++){
+  if($month > 12){
+    $year++;
+    $month -= 12;
+  }
+  $pullDownName[$i] = $year . '年' . $month . '月';
+  $pullDownValueY[$i] = $year;
+  $pullDownValueM[$i] = $month;
+  $month++;
+}
+echo '<form method="POST" action="/AMS/php/rensyu01.php">';
+echo '<select name="YM"> ';
+for($i=1 ; $i<4 ; $i++){
+//  echo $pullDownName[$i] ."<br>";
+$tmp = $pullDownValueY[$i] . sprintf('%02d', $pullDownValueM[$i]);
+  echo "<option value=\"" . $tmp ."\">". $pullDownName[$i] ."</option>";
+}
+echo '</select>';
+echo '<input type="submit" value="表示"></form>';
+echo '<br>';
+
+var_dump($pullDownValueY);
+
+if(isset($_POST['YM'])){
+  $_year = substr($_POST['YM'] ,0,4);
+  $_month = substr($_POST['YM'] ,4);
+}else{
+  $_year = $pullDownValueY[0];
+  $_month = $pullDownValueM[0];
+}
+echo '<br>'. $_year . '<br>';
+echo $_month . '<br>';
+
+//echo $pullDownName;
+
+/*//pass
 echo password_hash("yda474", PASSWORD_DEFAULT);
-
+*/
 ?>
 </body>
 </html>
